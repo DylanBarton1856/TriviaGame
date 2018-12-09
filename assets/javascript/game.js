cf39e4595d8377eb4b3696774febbffdfabdb836
DBarton <42483562+DylanBarton1856@users.noreply.github.com>
2018-12-09 11:25:14 -0700

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,42 +9,41 @@
 // * Don't forget to include a countdown timer.
 
 // questions and answer choices
+var answersChosen = [];
 var questions = [
 	{
-		q1: "Which of these engines came equipped in the USDM Nissan 240SX",
-        a1: ['KA24DE','SR20DE','RB25DE','CA18DE'],
-        c1: 'KA24DE'
+		q1: "Which of these engines came equipped in the USDM Nissan 240SX?",
+		a1: ['KA24DE', 'SR20DE', 'RB25DE', 'CA18DE'],
+		c1: 'KA24DE'
 	},
 	{
-		q2: "Which year began the S14 generation in America",
-		a2: ['1992','1991','1995','1997'],
+		q2: "Which year began the S14 generation in America?",
+		a2: ['1992', '1991', '1995', '1997'],
 		c2: '1995'
 	}
 ];
 // console.log(questions);
 
-// appending questions to DOM
-function displayQuestion(){
-// $(".quizHolder").text(JSON.stringify(questions[0].question));
-			$('#q1').append(questions[0].q1 + '<br>' + '<br>');
-			$('#q2').append(questions[1].q2 + '<br>' + '<br>');
+// adding questions to DOM
+function gameStart() {
+	// $(".quizHolder").text(JSON.stringify(questions[0].question));
+	$('#q1').prepend(questions[0].q1 + '<br>' + '<br>');
+	$('#q2').prepend(questions[1].q2 + '<br>' + '<br>');
 }
 
 // turning answer choices into buttons, display on DOM
- function displayAnswer(){
-		 // Puts all remaining answers on the page with a different value
-		 for (var i = 0; i < 4; i++) {
-			 $('#q1').append("<input type='radio' name='answers1'/>" + questions[0].a1[i]);
-			 $('#q2').append("<input type='radio' name='answers2'/>" + questions[1].a2[i]);
-		 }
+for (var i = 0; i < 4; i++) {
+	$('#q1').append("<button>" + questions[0].a1[i]);
+	$('#q2').append("<button>" + questions[1].a2[i]);
 }
+// register button clicks.
+$("button").on('click', function () {
+	console.log(this.innerText);
+})
 
-// click event for user choice
-$('.quizHolder').onclick(event)
-
+// allow only one button selection per question.
 
 // submit button
-
 
 
 // countdown timer
@@ -59,6 +58,4 @@ $('.quizHolder').onclick(event)
 
 
 
-displayQuestion();
-
-displayAnswer();
+gameStart();
